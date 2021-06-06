27cacf71a8e92b5760267095f4a723c490081127
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-06 12:51:58 +0100

    working on this

---
+++ clicker.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Windows.h>
+
+using std::cout
+
+void description(){
+    cout << "X to enable right click\n";
+    cout << "Z to disable right click\n";
+};
+
+int main(){
+    description();
+    return 0;
+}
