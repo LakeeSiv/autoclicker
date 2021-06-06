62120c84a9ed2cfddf5a7d3c85328676d42acf42
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-06 13:06:33 +0100

    formatting

--- clicker.cpp
+++ clicker.cpp
@@ -3,21 +3,21 @@
 
 using std::cout;
 
-void description(){
+void description() {
     cout << "Hold down X to right click\n";
     cout << "Hold down Z to left click\n";
 };
 
-void clicker(){
-    for (;;){
-        if (GetKeyState('X') & 0x8000){
-            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0,0);
-            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0,0);
+void clicker() {
+    for (;;) {
+        if (GetKeyState('X') & 0x8000) {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
             Sleep(10);
         }
-        if (GetKeyState('Z') & 0x8000){
-            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0,0);
-            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0,0);
+        if (GetKeyState('Z') & 0x8000) {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
             Sleep(10);
         }
 
@@ -25,7 +25,7 @@ void clicker(){
 };
 
 
-int main(){
+int main() {
     description();
     clicker();
     return 0;
